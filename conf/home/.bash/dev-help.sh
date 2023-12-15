702fb410ee0f686cbbac8b2e9ec632e4d28ee9d8
s16121986 <32817157+s16121986@users.noreply.github.com>
2023-12-16 01:28:00 +0300

    Update dev-help.sh

--- conf/home/.bash/dev-help.sh
+++ conf/home/.bash/dev-help.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 echo "Usage:
-  chown:dev [dir]           Change all files owner to dev:dev
-  chown:www [dir]           Change all files owner to www-data:dev
-  chmod:r [dir]             Set readonly files permissions (0640, 0750 to folders)
-  chmod:w [dir]             Set writeable files permissions (0660, 0770 to folders)
-  laravel:boot [dir]        Set default owner & permissions to LaravelApp folders
-  laravel:mk:storage [dir]  Create storage folders
+  chown-dev [dir]           Change all files owner to dev:dev
+  chown-www [dir]           Change all files owner to www-data:dev
+  laravel-install [dir]     Set default owner & permissions to LaravelApp folders
   clear-logs [dir]          Disable nginx conf (default: ./storage/logs)
   nginxenconf <conf name>   Enable nginx conf
   nginxdisconf <conf name>  Disable nginx conf
   git-tag <version>         Update release version"
+  
+  
+#  chmod:r [dir]             Set readonly files permissions (0640, 0750 to folders)
+#  chmod:w [dir]             Set writeable files permissions (0660, 0770 to folders)
