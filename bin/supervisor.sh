6ea60741a382dce45e6fe592a042541159100710
s16121986 <32817157+s16121986@users.noreply.github.com>
2024-01-06 02:18:58 +0300

    Update supervisor.sh

--- bin/supervisor.sh
+++ bin/supervisor.sh
@@ -7,7 +7,6 @@ BOOT_PATH="/tmp/wsl-boot"
 if [ -z $(which supervisor) ]; then
   sudo apt -y install supervisor
   sudo systemctl enable supervisor
-  sudo cp "$BOOT_PATH/conf/supervisor.conf" /etc/supervisor/conf.d/oex-horison.conf
 else
   echo "SKIPPED: Supervisor already installed"
 fi
