9def3ee49654194d0c57cd1213e85c9c12778faa
s16121986 <32817157+s16121986@users.noreply.github.com>
2024-01-07 10:02:38 +0300

    Update git.sh

--- bin/git.sh
+++ bin/git.sh
@@ -4,8 +4,19 @@ BOOT_PATH="/tmp/wsl-boot"
 
 if [ ! -f ~/.gitconfig ]; then
   sudo apt -y install git
+  
   cp "$BOOT_PATH/conf/git/.gitconfig" ~
   cp "$BOOT_PATH/conf/git/.gitignore" ~
+
+  read -p "Enter your git login: " login
+  read -p "Enter your display name: " name
+  read -p "Enter your git email: " email
+  read -p "Enter your git token: " token
+
+  sed -i "s/:login/$login/g" ~/.gitconfig
+  sed -i "s/:name/$name/g" ~/.gitconfig
+  sed -i "s/:email/$email/g" ~/.gitconfig
+  sed -i "s/:token/$token/g" ~/.gitconfig
 else
   echo "SKIPPED: Git already installed"
 fi
