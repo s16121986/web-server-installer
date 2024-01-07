d53ac9ef31594aed2dc7eb2d6e4f6ee1ee0baa4f
Гурьянов Сергей <s16121986@yandex.ru>
2024-01-07 15:40:31 +0300

    refactor: upd

--- bin/help.sh
+++ bin/help.sh
@@ -8,7 +8,7 @@ source "${ROOT_PATH}/lib/package-helpers.sh"
 source "${ROOT_PATH}/lib/echo.sh"
 
 # Common usage
-echo "Web server Installer usage:"
+echo "Web server installer usage:"
 dotted_line "  wsi" "Run default installer"
 dotted_line "  wsi uninstall" "Uninstall this library"
 dotted_line "  wsi [<package>]" "Install specific package"
